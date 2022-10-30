31e5ffc4529d6656f6f96f1cead9100d6f9b397e
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-30 18:14:22 +0300

    commit2 bugs fixed

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        int tarih;
-        int zodyak;
-        System.out.println("Doğum tarihinizi giriniz:");
-        tarih= scanner.nextInt();
-        zodyak=tarih%12;
-        switch (zodyak){
+        int date;
+        int zodiac;
+        System.out.println("Enter your date of birth:");
+        date= scanner.nextInt();
+        zodiac=date%12;
+        switch (zodiac){
             case 0:
-                System.out.println("Çin zodyağı burcunuz:Maymun");
+                System.out.println("Your Chinese zodiac sign: Monkey");
 break;
             case 1:
-                System.out.println("Çin zodyağı burcunuz:Horoz");
+                System.out.println("Your Chinese zodiac sign: Cock");
                 break;
             case 2:
-                System.out.println("Çin zodyağı burcunuz:Köpek");
+                System.out.println("Your Chinese zodiac sign: Dog");
                 break;
             case 3:
-                System.out.println("Çin zodyağı burcunuz:Domuz");
+                System.out.println("Your Chinese zodiac sign: Pig");
                 break;
             case 4:
-                System.out.println("Çin zodyağı burcunuz:Fare");
+                System.out.println("Your Chinese zodiac sign: Mouse");
                 break;
             case 5:
-                System.out.println("Çin zodyağı burcunuz:Öküz");
+                System.out.println("Your Chinese zodiac sign: Ox");
                 break;
             case 6:
-                System.out.println("Çin zodyağı burcunuz:Kaplan");
+                System.out.println("Your Chinese zodiac sign: Tiger");
                 break;
             case 7:
-                System.out.println("Çin zodyağı burcunuz:Tavşan");
+                System.out.println("Your Chinese zodiac sign: Rabbit");
                 break;
             case 8:
-                System.out.println("Çin zodyağı burcunuz:Ejderha");
+                System.out.println("Your Chinese zodiac sign: Dragon");
                 break;
             case 9:
-                System.out.println("Çin zodyağı burcunuz:Yılan");
+                System.out.println("Your Chinese zodiac sign: Snake");
                 break;
             case 10:
-                System.out.println("Çin zodyağı burcunuz:At");
+                System.out.println("Your Chinese zodiac sign: Horse");
                 break;
             case 11:
-                System.out.println("Çin zodyağı burcunuz:Koyun");
+                System.out.println("Your Chinese zodiac sign: Sheep");
         }
     }
 }
